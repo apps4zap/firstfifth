ae3676432c87085abe3c02561eff0c071faa9b2b
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-07-09 10:15:19 -0500

    Create physical.js

---
+++ studio/schemas/objects/physical.js
@@ -0,0 +1,44 @@
+// The schema for Character physical object type
+export default {
+  title: 'Physical',
+  name: 'physical',
+  type: 'object',
+  fields: [
+    {
+      title: 'Physicality',
+      name: 'physicality',
+      type: 'number'
+    },
+    {
+      title: 'Age',
+      name: 'age',
+      type: 'number'
+    },
+    {
+      title: 'Gender',
+      name: 'gender',
+      type: 'array',
+      of: [{type: "string"}]
+    },
+   {
+     title: 'Government tag',
+  name: 'gtag',
+  type: 'string'
+    },
+    {
+      title: 'Origin',
+      name: 'origin',
+      type: 'string'
+    },
+    {
+      title: 'Height',
+      name: 'height',
+      type: 'number'
+    },
+{
+      title: 'Length',
+      name: 'length',
+      type: 'number'
+    },
+  ]
+}
