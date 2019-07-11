cf9e0977a556313307bc360f60f2fd5c084d2163
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-07-11 10:03:32 -0500

    Create photos.js

---
+++ studio/schemas/objects/photos.js
@@ -0,0 +1,23 @@
+export default {
+  title: 'Photos',
+  name: 'photos',
+  type: 'array',
+  of: [{type: 'image'}]
+    fields: [
+    {
+  title: 'Photo1',
+  name: 'photo1',
+  type: 'image'
+     },
+     {
+     	title: 'Photo2',
+         name: 'photo2',
+         type: 'image'
+      },
+      {
+         title: 'Photo3',
+         name: 'photo3',
+         type: 'name'
+      }
+      ]
+   }
