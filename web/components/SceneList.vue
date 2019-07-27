91eeed27b925b966bedd42cea2ae1b2f0e7e0c24
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-07-27 08:54:07 -0500

    Create SceneList.vue

---
+++ web/components/SceneList.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul class="sceneList">
+    <SessionItem
+      v-for="(scheduleItem, index) in program.schedule"
+      :key="sceneItem._key"
+      :index="index"
+      :scenee-item="scheduleItem"
+    />
+  </ul>
+</template>
+<script>
+import SceneItem from '~/components/SceneItem'
+export default {
+  components: {
+    SceneItem
+  },
+  props: {
+    info: {
+      type: Object,
+      default: () => {},
+    },
+    program: {
+      type: Object,
+      default: () => {},
+      scene: {
+        type: Array,
+        default: () => []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import '../styles/custom-properties.css';
+.sceneList {
+  margin: 0;
+  padding: 0;
+}
+</style>
