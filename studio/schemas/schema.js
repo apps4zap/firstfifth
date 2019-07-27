27b6bffa3026da820aaff15eaec17c257b5f7baf
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-07-27 10:02:44 -0500

    +chapterInformation

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -42,6 +42,7 @@ export default createSchema({
     venue,
     session,
     scene,
+    chapterInformation
 
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
