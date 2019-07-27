9aee970e52c3e57ac0b255da57da70d24654ae51
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-07-27 10:11:01 -0500

    Create location.js

---
+++ studio/schemas/objects/location.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'venue',
+  type: 'object',
+  title: 'Venue',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name of venue'
+    },
+    {
+      name: 'city',
+      type: 'string',
+      title: 'City'
+    },
+    {
+      name: 'region',
+      type: 'string',
+      title: 'Region'
+    }
+  ]
+}
