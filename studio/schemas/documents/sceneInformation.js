66b76d0ff6e9e9c27f3584659e8a437e3b8c5a44
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-07-27 13:31:53 -0500

    Create sceneInformation.js

---
+++ studio/schemas/documents/sceneInformation.js
@@ -0,0 +1,58 @@
+export default {
+  name: 'sceneInformation',
+  type: 'document',
+  title: 'Scene Information',
+  __experimental_actions: [/*'create',*/ 'update', /*'delete',*/ 'publish'],
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Scene name'
+    },
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Description',
+      description: 'Synopsis'
+    },
+    {
+      name: 'image',
+      type: 'mainImage',
+      title: 'Chapter Image'
+    },
+    {
+      name: 'location',
+      type: 'location',
+      title: 'Location',
+      description: 'Where does the action take place?'
+    },
+    {
+      name: 'chapterlink',
+      type: 'url',
+      title: 'Chapter link'
+    },
+    {
+      name: 'keywords',
+      type: 'array',
+      title: 'Keywords',
+      description: 'Add keywords that describes the chapter.',
+      of: [{type: 'string'}],
+      options: {
+        layout: 'tags'
+      }
+    },
+    {
+      name: 'cast',
+      type: 'array',
+      description: 'Publish one or more persons and set a reference to them here.',
+      title: 'Cast',
+      validation: Rule => Rule.unique().error('You can only have one of a person'),
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'person'}]
+        }
+      ]
+    }
+  ]
+}
