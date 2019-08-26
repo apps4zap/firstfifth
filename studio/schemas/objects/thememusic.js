01bdc63a91fc7b7b91b19d138c3621139dfc34fc
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-08-26 16:01:00 -0500

    Create thememusic.js

---
+++ studio/schemas/objects/thememusic.js
@@ -0,0 +1,12 @@
+export default {
+  title: 'Theme Music',
+  name: 'thememusic',
+  type: 'file',
+  fields: [
+    {
+      name: 'description',
+      type: 'string',
+      title: 'Description'
+    }
+  ]
+}
