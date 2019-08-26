c394a92f0d9f140a8e12da68726e19e8ee6cafeb
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-08-26 15:46:59 -0500

    Create author.js

---
+++ studio/schemas/documents/author.js
@@ -0,0 +1,44 @@
+export default {
+  type: 'document',
+  name: 'author',
+  title: 'Author',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string'
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      description: 'Some frontends will require a slug to be set to be able to show the person',
+      options: {
+        source: 'name',
+        maxLength: 96
+      }
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'mainImage'
+    },
+    {
+      name: 'thememusic',
+      title: 'Theme Music',
+      type: 'thememusic'
+    },
+    {
+      name: 'bio',
+      type: 'bioPortableText',
+      title: 'Biography'
+    }
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      subtitle: 'slug.current',
+      media: 'image'
+    }
+  }
+}
